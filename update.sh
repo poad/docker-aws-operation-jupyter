7161324e59486e038027d196dd223b09ef6ff3fa
Kenji Saito <ken-yo@mbr.nifty.com>
2023-03-15 01:25:24 +0900

    Bumps node modules

---
+++ update.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+CUR=$(pwd)
+
+CURRENT=$(cd $(dirname $0);pwd)
+echo "${CURRENT}"
+
+cd "${CURRENT}"
+git pull --prune
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}"
+  exit $result
+fi
+pwd
+cd "${CURRENT}/assets"
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}"
+  exit $result
+fi
+yarn install && yarn upgrade
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}"
+  exit $result
+fi
+git commit -am "Bumps node modules" && git push
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}"
+  exit $result
+fi
+
+cd "${CUR}"
